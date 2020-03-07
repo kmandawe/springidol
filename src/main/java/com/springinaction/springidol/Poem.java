ddebf74390d164b7759811fbb1c35b5ec754959d
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-03-07 19:32:23 +0800

    1.6 - PoeticJuggler and Sonnet29 initial

--- src/main/java/com/springinaction/springidol/Poem.java
+++ src/main/java/com/springinaction/springidol/Poem.java
@@ -1,7 +1,5 @@
-//<start id="poem_java" /> 
 package com.springinaction.springidol;
 
 public interface Poem {
   void recite();
 }
-//<end id="poem_java" />
--- src/main/java/com/springinaction/springidol/PoeticJuggler.java
+++ src/main/java/com/springinaction/springidol/PoeticJuggler.java
@@ -1,23 +1,22 @@
-//<start id="poeticjuggler_java" /> 
 package com.springinaction.springidol;
 
 public class PoeticJuggler extends Juggler {
   private Poem poem;
 
-  public PoeticJuggler(Poem poem) { //<co id="co_injectPoem"/>
+  public PoeticJuggler(Poem poem) {
     super();
     this.poem = poem;
   }
 
-  public PoeticJuggler(int beanBags, Poem poem) { // <co id="co_injectPoemBeanBags"/>
+  public PoeticJuggler(int beanBags, Poem poem) {
     super(beanBags);
     this.poem = poem;
   }
 
+  @Override
   public void perform() throws PerformanceException {
     super.perform();
     System.out.println("While reciting...");
     poem.recite();
   }
 }
-//<end id="poeticjuggler_java" />
