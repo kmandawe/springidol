5d95caab55d6d2fad3108ecc58a627c435f4bcb1
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-03-07 20:03:31 +0800

    1.8 - Singleton Stage - factory method

--- src/main/java/com/springinaction/springidol/Stage.java
+++ src/main/java/com/springinaction/springidol/Stage.java
@@ -1,16 +1,15 @@
-//<start id="stage_java" /> 
 package com.springinaction.springidol;
 
 public class Stage {
+
   private Stage() {
   }
 
   private static class StageSingletonHolder {
-    static Stage instance = new Stage(); //<co id="co_lazyLoad"/>
+    static Stage instance = new Stage();
   }
 
   public static Stage getInstance() {
-    return StageSingletonHolder.instance; //<co id="co_returnInstance"/>
+    return StageSingletonHolder.instance;
   }
 }
-//<end id="stage_java" />
