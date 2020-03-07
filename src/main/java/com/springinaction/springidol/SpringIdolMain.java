1242cbfbaa83c42742da398a2f04f50fb8af2ee9
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-03-07 19:13:29 +0800

    1.4 - Exercise 2.1 [Solution]

---
+++ src/main/java/com/springinaction/springidol/SpringIdolMain.java
@@ -0,0 +1,14 @@
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringIdolMain {
+
+  public static void main(String[] args) throws PerformanceException {
+    ApplicationContext ctx =
+        new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");
+    Performer performer = (Performer) ctx.getBean("duke");
+    performer.perform();
+  }
+}
