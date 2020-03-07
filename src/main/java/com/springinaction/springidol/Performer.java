3f1dc36cdc3690158a8654579495380939c59935
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-03-07 18:18:03 +0800

    1.1 - Performer Interface

--- src/main/java/com/springinaction/springidol/Performer.java
+++ src/main/java/com/springinaction/springidol/Performer.java
@@ -1,7 +1,6 @@
-//<start id="performer_java" /> 
 package com.springinaction.springidol;
 
 public interface Performer {
   void perform() throws PerformanceException;
 }
-//<end id="performer_java" /> 
+
