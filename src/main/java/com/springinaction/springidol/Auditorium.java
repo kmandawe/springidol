7e33a56b8e9ce0de09d8783c23d592258e01c266
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-03-09 13:03:37 +0800

    1.11 Auditorium init-method and destroy-method demo

---
+++ src/main/java/com/springinaction/springidol/Auditorium.java
@@ -0,0 +1,13 @@
+package com.springinaction.springidol;
+
+public class Auditorium {
+
+  public void turnOnLights() {
+    System.out.println("Turning on the lights...");
+  }
+
+  public void turnOffLights() {
+    System.out.println("Turning off the lights...");
+  }
+
+}
--- src/main/java/com/springinaction/springidol/SpringIdolMain.java
+++ src/main/java/com/springinaction/springidol/SpringIdolMain.java
@@ -1,13 +1,14 @@
 package com.springinaction.springidol;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringIdolMain {
 
   public static void main(String[] args) throws PerformanceException {
-    ApplicationContext ctx =
+    AbstractApplicationContext ctx =
         new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");
+    ctx.registerShutdownHook();
     Stage theStage = (Stage) ctx.getBean("theStage");
     System.out.println(theStage != null);
   }
